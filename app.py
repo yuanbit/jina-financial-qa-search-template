55729756ca7367c651aaa8693816ad14beaba4d9
Bithiah Yuan <bithiahy@gmail.com>
2020-12-28 16:56:59 +0100

    add templates

---
+++ app.py
@@ -0,0 +1,39 @@
+__version__ = '0.0.1'
+
+import os
+import sys
+
+
+def config():
+    parallel = 1 if sys.argv[1] == 'index' else 1
+    shards = 1
+
+    os.environ['JINA_PARALLEL'] = str(parallel)
+    os.environ['JINA_SHARDS'] = str(shards)
+    os.environ['WORKDIR'] = './workspace'
+    os.makedirs(os.environ['WORKDIR'], exist_ok=True)
+    os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(65481))
+    os.environ['JINA_DATA_PATH'] = 'dataset/test_answers.csv'
+
+
+def dryrun():
+    f = Flow().load_config("flows/index.yml")
+    with f:
+        f.dry_run()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('choose between "index/search/dryrun" mode')
+        exit(1)
+    if sys.argv[1] == 'index':
+        config()
+        index()
+    elif sys.argv[1] == 'search':
+        config()
+        search()
+    elif sys.argv[1] == "dryrun":
+        config()
+        dryrun()
+    else:
+        raise NotImplementedError(f'unsupported mode {sys.argv[1]}')
